59c7d423ab9512d621d550cf5609de6bab3ef996
Anahi Quintero <anitaquintero99@gmail.com>
2022-03-06 18:18:28 -0600

    Fetch  API using async/await

--- js/app.js
+++ js/app.js
@@ -4,18 +4,19 @@ const APY_KEY = "af5566e9";
 const searchInput = document.querySelector("#search");
 const titleList = document.querySelector("#title-list");
 
-const searchMovie = () => {
-  const search = searchInput.value;
-  fetch(`${API_URL}?apikey=${APY_KEY}&s=${search}`)
-    .then((response) => response.json())
-    .then((json) => {
-      const { Search: results } = json;
-      titleList.innerHTML = "";
-      results.forEach(({ Title: title }) => {
-        titleList.insertAdjacentHTML("beforeend", `<li>${title}</li>`);
-      });
-    })
-    .catch((error) => console.error(error));
+const searchMovie = async () => {
+  try {
+    const search = searchInput.value;
+    const response = await fetch(`${API_URL}?apikey=${APY_KEY}&s=${search}`);
+    const json = await response.json();
+    const { Search: results } = json;
+    titleList.innerHTML = "";
+    results.forEach(({ Title: title }) => {
+      titleList.insertAdjacentHTML("beforeend", `<li>${title}</li>`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 searchInput.addEventListener("keyup", searchMovie);
