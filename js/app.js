a87dcdba011fc2eb4991708085ac9ec61f9a690c
Anahi Quintero <anitaquintero99@gmail.com>
2022-03-06 18:49:40 -0600

    Manage errors

--- js/app.js
+++ js/app.js
@@ -7,10 +7,29 @@ const titleList = document.querySelector("#title-list");
 const searchMovie = async () => {
   try {
     const search = searchInput.value;
+    titleList.innerHTML = "";
+
+    if (!search) return;
+    if (search?.length < 3) {
+      titleList.insertAdjacentHTML(
+        "beforeend",
+        "<li>Escribe minimo 3 letras</li>"
+      );
+      return;
+    }
+
     const response = await fetch(`${API_URL}?apikey=${APY_KEY}&s=${search}`);
     const json = await response.json();
     const { Search: results } = json;
-    titleList.innerHTML = "";
+
+    if (!results) {
+      titleList.insertAdjacentHTML(
+        "beforeend",
+        "<li>No se encontraron resultados</li>"
+      );
+      return;
+    }
+
     results.forEach(({ Title: title }) => {
       titleList.insertAdjacentHTML("beforeend", `<li>${title}</li>`);
     });
